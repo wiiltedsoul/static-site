06279e5110f0b5dddbc33f0a60c2988c701d412e
wasabii <wasabiizombie@gmail.com>
2025-07-20 14:49:30 -0500

    Add initial CSS styles for the application
    - Set body background color and text color for better readability
    - Define styles for headings (h1-h6) with specific font sizes and text shadows
    - Style links with hover effects and underline
    - Add styles for lists, code blocks, and blockquotes for improved presentation
    - Include custom scrollbar styles for a cohesive look
    - Ensure images are responsive with rounded borders and shadows

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
-from textnode import TextNode, TextType
+import shutil
+from pathlib import Path
 
+def recursive_copy(src, dst):
+    if not dst.is_dir():
+        dst.mkdir(parents=True, exist_ok=True)
+    for item in src.iterdir():
+        if item.is_dir():
+            new_dst = dst / item.name
+            new_dst.mkdir(exist_ok=True)
+            recursive_copy(item, new_dst)
+        else:
+            shutil.copy2(item, dst / item.name)
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
-
-
-main()
+    src = Path("static")
+    dst = Path("public")
+    if dst.exists():
+        shutil.rmtree(dst)
+    recursive_copy(src, dst)
