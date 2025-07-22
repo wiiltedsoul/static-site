4789341fa2d195cd3673a398ec40514855336f23
wasabii <wasabiizombie@gmail.com>
2025-07-21 23:38:24 -0500

    Fix basepath replacement in HTML template for href and src attributes using f-strings

--- src/gencontent.py
+++ src/gencontent.py
@@ -30,8 +30,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
-    template = template.replace('href="/', "href={basepath}")
-    template = template.replace('src="/', "src={basepath}")
+    template = template.replace('href="/', f"href={basepath}")
+    template = template.replace('src="/', f"src={basepath}")
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
